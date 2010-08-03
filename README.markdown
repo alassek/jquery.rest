8e9f84984daa458defc62a0739a818c79af49179
Adam Lassek <alassek@lyconic.com>
2010-08-02 20:57:17 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,58 @@
+## jQuery.rest ##
+
+A plugin to ease AJAX interaction with RESTful APIs such as Rails
+
+### Example ###
+
+Create a new 'task' record with a success callback
+
+    $.create(
+      '/tasks',
+      { description: 'do something' },
+      function (reponse) {
+        alert('successfully added task.');
+      }
+    );
+
+will result in:
+
+    [POST] '/tasks?description=do%20something'
+    
+Read an existing 'account' object and add it to the page (this callback is making some assumptions about your controller -- YMMV)
+
+    $.read(
+      '/accounts/2486',
+      function (response) {
+        $('ul#accounts').append(response);
+      }
+    );
+    
+    [GET] '/accounts/2486'
+
+Update an existing 'task' record with ID 54
+
+    $.update(
+      '/tasks/54',
+      { description: 'lunch tomorrow after 1pm' }
+    );
+    
+    [POST] '/tasks/54?_method=update&description=lunch%20tomorrow%20after%201pm'
+
+Update a nested 'task' record using dynamic IDs
+
+    $.update(
+      '/accounts/{account_id}/tasks/{id}',
+      { id: 54, account_id: 11387, description: 'lunch tomorrow after 1pm' }
+    );
+    
+    [POST] '/accounts/11387/tasks/54?_method=update&description=lunch%20tomorrow%20after%201pm'
+
+Delete a 'task' object with ID 54
+
+    $.destroy('/tasks/54')
+    
+    [POST] '/tasks/54?_method=delete'
+
+### Regarding authenticity_token ###
+
+Currently this assumes that you are putting the `authenticity_token` for the page in a JS variable called `window.AUTH_TOKEN`. This is simple enough to change if you want to do it differently.
---
+++ jquery.rest.js
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2010 Lyconic, LLC. 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+(function($){
+    
+    // jQuery doesn't provide a better way of intercepting the ajax settings object
+    var _ajax = $.ajax, options;
+    
+    var collect_options = function(url, data, callback, type) {
+        options = { dataType: 'json' };
+        if (arguments.length == 1) {
+            options = $.extend(options, url);            
+            if (options['url'] && options['data']) options.url = fill_url(options.url, options.data);
+        } else {
+            // shift arguments if data argument was omitted
+            if ($.isFunction(data) || $.isArray(data)) {
+                type = type || callback;
+                callback = data;
+                data = null;
+            }
+            
+            var success, error;
+            if ($.isArray(callback)) {
+                success = callback[0];
+                error = callback[1];
+            } else {
+                success = callback;
+            }
+            
+            url = fill_url(url, data);
+            
+            options = $.extend(options, {
+                url: url,
+                data: data,
+                success: success,
+                error: error,
+                dataType: type
+            });
+        }
+    };
+    
+    var fill_url = function(url, data) {
+        var key, u, val;
+        for (key in data) {
+            val = data[key];
+            u = url.replace('{'+key+'}', val);
+            if (u != url) {
+                url = u;
+                delete data[key];
+            }
+        }
+        return url;
+    };
+    
+    $.extend({
+        ajax: function(settings) {
+            settings.type = settings.type || "GET";
+                
+            if (typeof settings.data !== "string")
+            if (settings.data !== undefined)
+            if (settings.data !== null) {
+                settings.data = $.param(settings.data);
+            }
+            
+            settings.data = settings.data || "";
+            
+            if (!/^(get)$/i.test(settings.type))
+            if (!/(authenticity_token=)/i.test(settings.data)) {
+                settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+            }
+            
+            if (!/^(get|post)$/i.test(settings.type)) {
+                settings.data += (settings.data ? "&" : "") + "_method=" + settings.type.toLowerCase();
+                settings.type = "POST";
+            }
+            
+            return _ajax.call(this, settings);
+        },
+        
+        read: function() {
+           collect_options.apply(this, arguments);
+           return $.ajax(options);
+        },
+        
+        create: function() {
+           collect_options.apply(this, arguments);
+           options = $.extend({ type: 'POST' }, options);
+           return $.ajax(options);
+        },
+        
+        update: function() {
+            collect_options.apply(this, arguments);
+            options = $.extend({ type: 'PUT' }, options);
+            return $.ajax(options);
+        },
+        
+        destroy: function() {
+            collect_options.apply(this, arguments);
+            options = $.extend({ type: 'DELETE' }, options);
+            return $.ajax(options);
+        }
+    });
+    
+})(jQuery);
